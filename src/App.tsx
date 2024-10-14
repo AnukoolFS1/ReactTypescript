4739c0cb55b91a0734119cde969ea5d289cf8814
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-10-14 18:55:13 +0530

    changed some directories

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Display from "./DisplayName"
+import Display from "./components/DisplayName"
 
 function App() {
 
   return (
     <>
      <h1>Hello! Typescript.</h1>
-     <Display name={'Anukool'} profession={"Full Stack Developer"} age={25}></Display>
+     <Display name={'Anukool'} profession={"Full Stack Developer"} age={25} />
     </>
   )
 }
